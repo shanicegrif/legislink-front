808607e582c2aa271418d364a7ba2d81720ba31f
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2024-02-28 19:28:35 -0500

    adds react-select

--- src/components/home/TopCarousel.jsx
+++ src/components/home/TopCarousel.jsx
@@ -5,21 +5,42 @@ export default function TopCarousel(){
         <div>
             <Carousel fade>
                 <Carousel.Item>
-                    <img src='https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D' text="First slide" />
+                    <img 
+                        src='https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=1xw:0.84415xh;center,top' 
+                        text="First slide"
+                        style={{maxHeight:"600px",
+                                maxWidth:"800px",
+                                minHeight:"600px",
+                                minWidth:"800px",}}
+                    />
                     <Carousel.Caption>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src='https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D' text="First slide" />
+                    <img 
+                        src='https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D' 
+                        text="First slide"
+                        style={{maxHeight:"600px",
+                                maxWidth:"800px",
+                                minHeight:"600px",
+                                minWidth:"800px",}} 
+                    />
                     <Carousel.Caption>
                         <h3>Second slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src='https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D' text="First slide" />
+                    <img 
+                        src='https://media.istockphoto.com/id/1361394182/photo/funny-british-shorthair-cat-portrait-looking-shocked-or-surprised.jpg?s=612x612&w=0&k=20&c=6yvVxdufrNvkmc50nCLCd8OFGhoJd6vPTNotl90L-vo=' 
+                        text="First slide"
+                        style={{maxHeight:"600px",
+                                maxWidth:"800px",
+                                minHeight:"600px",
+                                minWidth:"800px",}}
+                    />
                     <Carousel.Caption>
                         <h3>Third slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
