8bd6e945f74cab9bce875d4462e35d4b38621799
Genesis Lara <genesislara@pursuit.org>
2024-03-20 15:05:24 -0400

    filter through bills

--- src/pages/Bills.jsx
+++ src/pages/Bills.jsx
@@ -6,27 +6,48 @@ import { useLocation } from "react-router-dom";
 
 export default function Bills() {
     const [bills, setBills] = useState([]);
+    const [keyword, setKeyword] = useState('');
     let location = useLocation();
 
+
+    const fetchBillsByKeyword = async () => {
+        try {
+            const res = await fetchForBills(keyword);
+            setBills(res.data.results[0].bills);
+
+        } catch (error) {
+            console.error('Error fetching bills:', error);
+        }
+    };
+
     useLayoutEffect(() => {
-        fetchForBills().then((res) => {
-            setBills(res.data.results[0].bills)
-        }).catch(function (error) {
-            if (error.response) {
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-            } else if (error.request) {
-                console.log(error.request);
-            } else {
-                console.log('Error', error.message);
-            }
-        });
-    }, [location]);
+        fetchBillsByKeyword();
+    }, [keyword, location]);
+
+    // useLayoutEffect(() => {
+    //     fetchForBills().then((res) => {
+    //         setBills(res.data.results[0].bills)
+    //     }).catch(function (error) {
+    //         if (error.response) {
+    //             console.log(error.response.data);
+    //             console.log(error.response.status);
+    //             console.log(error.response.headers);
+    //         } else if (error.request) {
+    //             console.log(error.request);
+    //         } else {
+    //             console.log('Error', error.message);
+    //         }
+    //     });
+    // }, [location]);
     return (
         <div>
+            <input type="text" value={keyword} onChange={(e) => setKeyword(e.target.value)} placeholder="Enter keyword for filtering" />
+            <button>Search</button>
             <h1>
-                {bills.map((elem) => <BillsCard bill={elem} />)}
+                {bills.map((elem, index) => ( 
+                <BillsCard key={index} bill={elem} />
+                ))}
+
             </h1>
         </div>
         );
