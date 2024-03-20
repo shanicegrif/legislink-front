8ad280f314b0c1dd5b57fa4d48f0ea33a73743c0
Genesis Lara <genesislara@pursuit.org>
2024-03-20 19:51:13 -0400

    filters through bills correctly

--- src/pages/Bills.jsx
+++ src/pages/Bills.jsx
@@ -1,14 +1,11 @@
-import { useLayoutEffect, useState } from "react";
+import { useState } from "react";
 import { fetchForBills } from "../api/axios";
 import BillsCard from "../components/bills/billscard";
-import { useLocation } from "react-router-dom";
 
 
 export default function Bills() {
     const [bills, setBills] = useState([]);
     const [keyword, setKeyword] = useState('');
-    let location = useLocation();
-
 
     const fetchBillsByKeyword = async () => {
         try {
@@ -20,34 +17,18 @@ export default function Bills() {
         }
     };
 
-    useLayoutEffect(() => {
+    const handleSearch = () => {
         fetchBillsByKeyword();
-    }, [keyword, location]);
+    }; 
 
-    // useLayoutEffect(() => {
-    //     fetchForBills().then((res) => {
-    //         setBills(res.data.results[0].bills)
-    //     }).catch(function (error) {
-    //         if (error.response) {
-    //             console.log(error.response.data);
-    //             console.log(error.response.status);
-    //             console.log(error.response.headers);
-    //         } else if (error.request) {
-    //             console.log(error.request);
-    //         } else {
-    //             console.log('Error', error.message);
-    //         }
-    //     });
-    // }, [location]);
     return (
         <div>
             <input type="text" value={keyword} onChange={(e) => setKeyword(e.target.value)} placeholder="Enter keyword for filtering" />
-            <button>Search</button>
+            <button onClick={handleSearch}>Search</button>
             <h1>
                 {bills.map((elem, index) => ( 
                 <BillsCard key={index} bill={elem} />
                 ))}
-
             </h1>
         </div>
         );
