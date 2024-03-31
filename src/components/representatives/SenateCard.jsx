e28cf934989c2c434f4c7b730c464ccdccc722c5
Shanice <shanicegriffin@pursuit.org>
2024-03-31 10:28:17 -0400

    rep route now show both house and senate

---
+++ src/components/representatives/SenateCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const SenateCard = ({ representative }) => {
+  return (
+    <div className="col-lg-3 col-md-4 col-sm-6 mb-4 d-flex">
+      <Card sx={{ maxWidth: 145, display: 'flex', flexDirection: 'column' }}>
+        <CardMedia
+          sx={{ height: 160 }}
+          image={`https://www.congress.gov/img/member/${representative.id.toLowerCase()}_200.jpg`}
+          title="senate image"
+        />
+        <CardContent style={{ flex: '1' }}>
+          <Typography gutterBottom variant="h5" component="div">
+            {representative.first_name} {representative.last_name}
+          </Typography>
+          <Typography variant="body2">Party: {representative.party}</Typography>
+          <Typography variant="body2">State: {representative.state}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small"><Link to={`/representatives/${representative.id}`} state={{representative:representative}}>Learn More</Link></Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
+
+export default SenateCard;
