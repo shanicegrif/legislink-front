203005970afcb3ea95649746a319abcd5ebc4f66
Shanice <shanicegriffin@pursuit.org>
2024-04-01 21:02:14 -0400

    changed layout of rep cards and detail card

--- src/components/representatives/RepresentativeCard.jsx
+++ src/components/representatives/RepresentativeCard.jsx
@@ -5,50 +5,70 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import "./RepSenCard.css";
+
+const CustomTypography = styled.div`
+  font-family: "Libre Baskerville", serif;
+  font-weight: bold;
+  font-size: 22px;
+`;
+
 
 export default function RepresentativeCard({ representative }) {
   return (
     <div className="card-item">
       <Card
         sx={{
-          maxWidth: 200,
-          minWidth: 200,
+          minWidth: 350,
+          maxWidth: 250,
           display: "flex",
-          flexDirection: "column",
+          flexDirection: "row",
+          boxShadow:
+            representative.party === "R"
+              ? "2px 2px 2px 2px red"
+              : representative.party === "D"
+              ? "2px 2px 2px 2px #2366c8"
+              : "2px 2px 2px 2px grey",
         }}
       >
         <CardMedia
-          sx={{ width: 200, height: 200 }}
+          sx={{ width: 250, height: 200, maxWidth: 150 }}
           image={`https://www.congress.gov/img/member/${representative.id.toLowerCase()}_200.jpg`}
           title="senate image"
         />
         <div className="card-content">
-          <CardContent style={{ flex: "1" }}>
-            <Typography gutterBottom variant="h5" component="div">
+          <CardContent
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-evenly",
+              minHeight: 150,
+              padding: 1, // Remove default padding
+              paddingLeft: "10px",
+            }}
+          >
+            <CustomTypography>
               {representative.first_name} {representative.last_name}
-            </Typography>
+            </CustomTypography>
             <Typography variant="body2">
               Party: {representative.party}
             </Typography>
             <Typography variant="body2">
               District: {representative.district}
             </Typography>
-            <Typography variant="body2">
-              Office: {representative.office}
-            </Typography>
           </CardContent>
+          <CardActions>
+            <Button size="small">
+              <Link
+                to={`/representatives/${representative.id}`}
+                state={{ representative: representative }}
+              >
+                Learn More
+              </Link>
+            </Button>
+          </CardActions>
         </div>
-        <CardActions>
-          <Button size="small">
-            <Link
-              to={`/representatives/${representative.id}`}
-              state={{ representative: representative }}
-            >
-              Learn More
-            </Link>
-          </Button>
-        </CardActions>
       </Card>
     </div>
   );
