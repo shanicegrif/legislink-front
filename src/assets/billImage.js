638d95891d3e0ed79ed71c331e679215ef388a4b
Shanice <shanicegriffin@pursuit.org>
2024-04-08 21:46:08 -0400

    started set up for bill images

---
+++ src/assets/billImage.js
@@ -0,0 +1,31 @@
+import unknown from "./unknown.jpeg";
+import judiciary from "./judiciary.jpg"
+import pension from "./pension.jpg"
+import banking from "./banking.jpg"
+import housing from "./housing.jpg"
+import urban from "./urban.jpg"
+import tech from "./tech.png"
+import agricultural from "./agricultural.jpg"
+import works from "./works.png"
+import environment from "./environment.png"
+import health from "./health.png"
+import natural from "./natural.png"
+import veterans from "./veterans.png"
+
+const billImage = {
+    unknown: unknown,
+    judiciary: judiciary,
+    pension: pension,
+    banking: banking,
+    housing: housing,
+    urban: urban,
+    tech: tech,
+    agricultural: agricultural,
+    works: works,
+    environment: environment,
+    health: health,
+    natural: natural,
+    veterans: veterans,
+};
+
+export default billImage;
