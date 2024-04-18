eb05f03e7d32d16fd0a421539df5e2098e5e23f1
Shanice <shanicegriffin@pursuit.org>
2024-04-18 17:10:13 -0400

    fixed bill border and icons

--- src/components/bills/repBillCard.jsx
+++ src/components/bills/repBillCard.jsx
@@ -5,6 +5,8 @@ import CardContent from "@mui/material/CardContent";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import styled from "styled-components";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
 import iconImage from "../Icons";
 
 const CustomTypography = styled.div`
@@ -22,7 +24,9 @@ const CustomTypographyTwo = styled.div`
 `;
 
 const ListWrapper = styled.div`
-  margin-top: auto; // Align the list items to the bottom of the card
+  margin-top: 20px; // Align the list items to the bottom of the card
+  margin-bottom: -25px;
+  margin-left: -20px;
 `;
 
 const CustomListItemText = styled.div`
@@ -34,6 +38,12 @@ const CustomListItemText = styled.div`
 
 const RepBillCard = ({ bill, onClick }) => {
   let majorAction = bill.latest_major_action.split("Committee on").pop();
+  const maxTextLength = 50;
+
+  const truncatedMajorAction =
+    majorAction && majorAction.length > maxTextLength
+      ? majorAction.slice(0, maxTextLength) + "..."
+      : majorAction;
 
   const foundActionImage = (majorAction) => {
     if (majorAction) {
@@ -54,19 +64,32 @@ const RepBillCard = ({ bill, onClick }) => {
         const lowerWord = word.toLowerCase();
         for (const key of Object.keys(iconImage)) {
           if (lowerWord.includes(key.replace("_", " "))) {
-            return iconImage[key];
+            return { name: key, icon: iconImage[key] };
           }
         }
       });
       const filteredIcons = icons.filter((icon) => icon !== undefined);
-      return filteredIcons.length ? filteredIcons : [iconImage.unknown]; // Return unknown icon if no matches
+      return filteredIcons.length
+        ? filteredIcons
+        : [{ name: "unknown", icon: iconImage.unknown }]; // Return unknown icon if no matches
     }
-    return [iconImage.unknown]; // Return unknown icon if majorAction is falsy
+    return [{ name: "unknown", icon: iconImage.unknown }]; // Return unknown icon if majorAction is falsy
   };
-
   const icons = foundActionIcon(majorAction);
 
-  console.log(bill)
+  const getStatusColor = (bill) => {
+    if (bill.vetoed) {
+      return "red";
+    } else if (bill.passed) {
+      return "green";
+    } else if (bill.introduced_date) {
+      return "orange";
+    } else {
+      return "white"; // Default color
+    }
+  };
+  const statusColor = getStatusColor(bill);
+
   return (
     <Card
       sx={{
@@ -82,7 +105,7 @@ const RepBillCard = ({ bill, onClick }) => {
         image={foundActionImage(majorAction)}
         title="bill category image"
       />
-      <CardContent>
+      <CardContent sx={{ borderLeft: `5px solid ${statusColor}` }}>
         <CustomTypography variant="body2">
           Subject: {majorAction}
         </CustomTypography>
@@ -90,19 +113,22 @@ const RepBillCard = ({ bill, onClick }) => {
         <CustomTypographyTwo variant="h6" component="div">
           {bill.short_title}
         </CustomTypographyTwo>
+        <ListWrapper>
+          <div style={{ display: "flex", marginRight: "290px" }}>
+            {" "}
+            {icons.map((icon, index) => (
+              <ListItem key={index}>
+                <Tooltip title={icon.name} arrow>
+                  <IconButton>{icon.icon}</IconButton>
+                </Tooltip>
+              </ListItem>
+            ))}
+          </div>
+          <ListItem>
+            <CustomListItemText>{`Date: ${bill.latest_major_action_date}`}</CustomListItemText>
+          </ListItem>
+        </ListWrapper>
       </CardContent>
-      <ListWrapper>
-        <div style={{ display: "flex", marginRight: "300px" }}>
-          {" "}
-          {/* Use flexbox to display icons in a row */}
-          {icons.map((Icon, index) => (
-            <ListItem key={index}>{Icon}</ListItem>
-          ))}
-        </div>
-        <ListItem>
-          <CustomListItemText>{`Date: ${bill.latest_major_action_date}`}</CustomListItemText>
-        </ListItem>
-      </ListWrapper>
     </Card>
   );
 };
