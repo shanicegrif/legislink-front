c99d66233b6cfdbc73389b68d1d607f777b41156
Shanice <shanicegriffin@pursuit.org>
2024-06-21 18:41:47 -0400

    problem with nav

--- src/components/DynamicNav.jsx
+++ src/components/DynamicNav.jsx
@@ -1,28 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useAuth } from "../hooks/useAuth";
 import SideNav from './SideNav';
 import HomeNav from './HomeNav';
-import Loading from '../components/messages/Loading';
 
 const DynamicNav = () => {
     const auth = useAuth();
-    const [authResolved, setAuthResolved] = useState(false);
 
-    useEffect(() => {
-        if (auth !== null) {
-            setAuthResolved(true);
-        }
-    }, [auth]);
-
-    // Render loading indicator until auth state is resolved
-    if (!authResolved) {
-        return <Loading />;
-    }
-
-    // Once auth state is resolved, render appropriate nav
     return (
         <>
-            {!auth ? (<HomeNav />) : (<SideNav />)}
+            {!auth ? (<HomeNav />) : null}
+            {auth ? (<SideNav />) : null}
         </>
     );
 }
